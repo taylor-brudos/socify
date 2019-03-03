f3664d03c1845f15a1aceb0a2d6be677b6ca7d43
taylor-brudos <taylor.brudos@gmail.com>
2019-03-02 16:30:20 -0800

    generate button downloads xlsx file to client

--- main/apps/first_app/views.py
+++ main/apps/first_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.template import loader, Context
+import openpyxl
+import os
 
 def index(request):
     context = {
@@ -7,3 +10,18 @@ def index(request):
     }
     return render(request, "first_app/index.html", context)
     
+def generate(request):
+    if request.method == "POST":
+        print("*"*50 + "   Generate method invoked!")
+        print(os.getcwd())
+        wb = openpyxl.Workbook()
+        sheet = wb.active
+        sheet.title = "WP-01"
+        wb.save('testWorkPaper.xlsx')
+        file_path = 'testWorkPaper.xlsx'
+        response = HttpResponse(open(file_path, 'rb').read())
+        response['Content-Type'] = 'mimetype/submimetype'
+        response['Content-Disposition'] = 'attachment; filename=testWPdwnld.xlsx'
+        return response
+    else:
+        return redirect("/")
